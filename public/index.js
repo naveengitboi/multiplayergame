798b65c4d8d170b0ec0bdd7e53ebf1ce65b77461
naveengitboi <naveenjangiti9@gmail.com>
2024-12-15 04:44:05 +0530

    image broken error

--- public/index.js
+++ public/index.js
@@ -1,10 +1,48 @@
+const mapImage = new Image();
+mapImage.src = "../src/snowy-sheet.png";
+
+
+const canvasEle = document.getElementById("canvas");
+
+const ctx = canvasEle.getContext("2d");
+canvasEle.width = window.innerWidth;
+canvasEle.height = window.innerHeight;
+
 const socket = io("ws://localhost:3000");
 
 socket.on("connect", (response) => {
   console.log("Connectd", response);
 });
 
+let map = [[]];
+const TILE_SIZE = 16;
+const TILES_IN_ROW = 8;
+socket.on("map", (loadedMap) => {
+  map = loadedMap;
+});
 
-socket.on("map", (response) => {
-  console.log(response)
-})
+function loop() {
+  ctx.clearRect(0, 0, canvasEle.width, canvasEle.height);
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[0].length; col++) {
+      const { id } = map[row][col];
+      const imageRow = parseInt(id / TILES_IN_ROW);
+      const imageCol = parseInt(id % TILES_IN_ROW);
+      ctx.drawImage(
+        mapImage,
+        imageCol * TILE_SIZE,
+        imageRow * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+        col * TILE_SIZE,
+        row * TILE_SIZE,
+        TILE_SIZE,
+        TILE_SIZE,
+      );
+    }
+  }
+  ctx.fillRect(0, 0, canvasEle.width, canvasEle.height);
+  window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
